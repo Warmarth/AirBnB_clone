f72396e7a3ce4e1d2281b8d25ce330fc7657681f
Warmarth <godsonibeaka@gmail.com>
2022-11-23 05:54:54 -0800

    update

---
+++ models/base_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" defining the BaseModel Class"""
+
+import model
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel():
+
+    def __init__(self,):
+        
+
+    def id(self,id,uuid):
+        self.id = id
+        self.uuid = uuid
+
+
+
