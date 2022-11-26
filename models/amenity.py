7dff070baf12c4c1aca42441b237e3cc3f405891
Warmarth <godsonibeaka@gmail.com>
2022-11-26 00:59:45 +0000

    new update

---
+++ models/amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""`Amenity` class module"""
+
+
+
+from .base_model import BaseModel
+
+
+
+
+
+class Amenity(BaseModel):
+
+    """A class that represent an amenity"""
+
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""`City` class module"""
+
+
+
+from .base_model import BaseModel
+
+
+
+
+
+class City(BaseModel):
+
+    """A class that represent a city"""
+
+    state_id = ""
+
+    name = ""
